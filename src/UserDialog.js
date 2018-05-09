ffa01c78d700906a2423ea1f8363efdc2c998ef5
BassXiaoSen <314121378@qq.com>
2018-05-09 19:57:51 +0800

    受控登录注册表单

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -4,25 +4,53 @@ export default class UserDialog extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            selected: "signUp"
+            selected: "signUp",
+            formData:{
+                username:'',
+                password:''
+            }
         }
+    }
+    deepClone(state){
+        return JSON.parse(JSON.stringify(state))
     }
     switch = (e) => {
         this.setState({
             selected: e.target.value
         })
     }
+    signUp=(e)=>{
 
+    }
+    signIn=(e)=>{
+
+    }
+    changeUserName=(e)=>{
+        let stateCopy = this.deepClone(this.state)
+        stateCopy.formData.username = e.target.value
+        this.setState(stateCopy)
+    }
+    changePassword=(e)=>{
+        let stateCopy = this.deepClone(this.state)
+        stateCopy.formData.password = e.target.value
+        this.setState(stateCopy)
+    }
     render() {
         let signUpForm = (
-            <form className="signUp"> {/* 注册*/}
+            <form className="signUp" onSubmit={this.signUp}> {/* 注册*/}
                 <div className="row">
                     <label>用户名</label>
-                    <input type="text" />
+                    <input type="text" 
+                        value={this.state.formData.username}
+                        onChange={this.changeUserName}
+                    />
                 </div>
                 <div className="row">
                     <label>密码</label>
-                    <input type="password" />
+                    <input type="password" 
+                        value={this.state.formData.password}
+                        onChange={this.changePassword}
+                    />
                 </div>
                 <div className="row actions">
                     <button type="submit">注册</button>
@@ -30,14 +58,20 @@ export default class UserDialog extends React.Component {
             </form>
         )
         let signInForm = (
-            <form className="signIn"> {/* 登录*/}
+            <form className="signIn" onSubmit={this.signIn}> {/* 登录*/}
                 <div className="row">
                     <label>用户名</label>
-                    <input type="text" />
+                    <input type="text" 
+                        value={this.state.formData.username}
+                        onChange={this.changeUserName}
+                    />
                 </div>
                 <div className="row">
                     <label>密码</label>
-                    <input type="password" />
+                    <input type="password" 
+                        value={this.state.formData.password}
+                        onChange={this.changePassword}
+                    />
                 </div>
                 <div className="row actions">
                     <button type="submit">登录</button>
