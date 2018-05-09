9d0f945745774da0ff5dffb34fd78230b6c1ada0
BassXiaoSen <314121378@qq.com>
2018-05-09 18:44:18 +0800

    可切换登录注册

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,56 +1,59 @@
 import React from "react"
 import "./UserDialog.css"
 export default class UserDialog extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            selected:"signUp"
+        this.state = {
+            selected: "signUp"
         }
     }
-    switch=(e)=>{
+    switch = (e) => {
         this.setState({
-            selected:e.target.value
+            selected: e.target.value
         })
     }
 
     render() {
+        let signUpForm = (
+            <form className="signUp"> {/* 注册*/}
+                <div className="row">
+                    <label>用户名</label>
+                    <input type="text" />
+                </div>
+                <div className="row">
+                    <label>密码</label>
+                    <input type="password" />
+                </div>
+                <div className="row actions">
+                    <button type="submit">注册</button>
+                </div>
+            </form>
+        )
+        let signInForm = (
+            <form className="signIn"> {/* 登录*/}
+                <div className="row">
+                    <label>用户名</label>
+                    <input type="text" />
+                </div>
+                <div className="row">
+                    <label>密码</label>
+                    <input type="password" />
+                </div>
+                <div className="row actions">
+                    <button type="submit">登录</button>
+                </div>
+            </form>
+        )
         return (
             <div className="UserDialog-Wrapper">
                 <div className="UserDialog">
                     <nav onChange={this.switch}>
-                        <input type="radio" value="signUp" checked={this.state.selected==="signUp"}/>注册
-                        <input type="radio" value="signIn" checked={this.state.selected==='signIn'} />登录
+                        <label><input type="radio" value="signUp" checked={this.state.selected === "signUp"} />注册</label>
+                        <label><input type="radio" value="signIn" checked={this.state.selected === 'signIn'} />登录</label>
                     </nav>
                     <div className="panes">
-
-                        <form className="signUp"> {/* 注册*/}
-                            <div className="row">
-                                <label>用户名</label>
-                                <input type="text" />
-                            </div>
-                            <div className="row">
-                                <label>密码</label>
-                                <input type="password" />
-                            </div>
-                            <div className="row actions">
-                                <button type="submit">注册</button>
-                            </div>
-                        </form>
-
-                        <form className="signIn"> {/* 登录*/}
-                            <div className="row">
-                                <label>用户名</label>
-                                <input type="text" />
-                            </div>
-                            <div className="row">
-                                <label>密码</label>
-                                <input type="password" />
-                            </div>
-                            <div className="row actions">
-                                <button type="submit">登录</button>
-                            </div>
-                        </form>
-
+                        {this.state.selected === 'signUp'?signUpForm:null}
+                        {this.state.selected === 'signIn'?signInForm:null}                        
                     </div>
                 </div>
             </div>
