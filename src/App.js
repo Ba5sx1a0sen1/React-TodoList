d0a610751ba69315cd4270b740fda061fe71723f
BassXiaoSen <314121378@qq.com>
2018-05-15 22:13:08 +0800

    登录时获取各自用户的数据

--- src/App.js
+++ src/App.js
@@ -6,74 +6,83 @@ import './reset.css'
 import TodoInput from "./TodoInput"
 import TodoItem from "./ToduItem"
 import UserDialog from "./UserDialog"
-import {getCurrentUser,signOut,TodoModel} from './leancloud'
+import { getCurrentUser, signOut, TodoModel } from './leancloud'
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       user: getCurrentUser() || {},
-      newTodo:'',
-      todoList:[]
+      newTodo: '',
+      todoList: []
     }
     let user = getCurrentUser()
-    if(user){
-      TodoModel.getByUser(user,(todos)=>{
+    if (user) {
+      TodoModel.getByUser(user, (todos) => {
         let stateCopy = JSON.parse(JSON.stringify(this.state))
         stateCopy.todoList = todos
         this.setState(stateCopy)
       })
     }
   }
-  addTodo=(event)=>{
+  addTodo = (event) => {
     console.log('调用了')
-    let newTodo={
-      title:event.target.value,
-      status:'',
-      deleted:false,
+    let newTodo = {
+      title: event.target.value,
+      status: '',
+      deleted: false,
     }
-    TodoModel.create(newTodo,(id)=>{
+    TodoModel.create(newTodo, (id) => {
       newTodo.id = id
       this.state.todoList.push(newTodo)
       this.setState({
-        newTodo:'',
-        todoList:this.state.todoList
+        newTodo: '',
+        todoList: this.state.todoList
       })
-    },(error)=>{
+    }, (error) => {
       console.log(error)
     })
   }
-  changeTitle=(e)=>{
+  changeTitle = (e) => {
     this.setState({
-      newTodo:e.target.value,
+      newTodo: e.target.value,
       todoList: this.state.todoList
     })
   }
-  toggle=(e,todo)=>{
+  toggle = (e, todo) => {
     let oldStatus = todo.status
-    todo.status= todo.status === 'completed'?'':'completed'
-    TodoModel.update(todo,()=>{
+    todo.status = todo.status === 'completed' ? '' : 'completed'
+    TodoModel.update(todo, () => {
       this.setState(this.state)
-    },(error)=>{
+    }, (error) => {
       todo.status = oldStatus
       this.setState(this.state)
     })
     console.log(this.state)
   }
 
-  delete=(e,todo)=>{
+  delete = (e, todo) => {
     TodoModel.destroy(todo.id, () => {
       todo.deleted = true
       this.setState(this.state)
     })
-    console.log(this.state)    
+    console.log(this.state)
   }
-  onSignUpOrSignIn=(user)=>{
+  onSignUpOrSignIn = (user) => {
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.user = user
-    this.setState(stateCopy)
+    this.setState(stateCopy, () => {
+      let user = getCurrentUser()
+      if (user) {
+        TodoModel.getByUser(user, (todos) => {
+          let stateCopy = JSON.parse(JSON.stringify(this.state))
+          stateCopy.todoList = todos
+          this.setState(stateCopy)
+        })
+      }
+    })
   }
-  signOut=()=>{
+  signOut = () => {
     signOut()
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.user = {}
@@ -83,32 +92,32 @@ class App extends Component {
 
   render() {
     let todos = this.state.todoList
-      .filter((item)=>(item.deleted!==true))
-      .map((value,index)=>{//Array Api (value,index)
-      return (
-      <li key={index}>
-        <TodoItem todo={value} 
-        onDelete={this.delete}
-        onToggle={this.toggle}/>
-      </li>
-      )
-    })
+      .filter((item) => (item.deleted !== true))
+      .map((value, index) => {//Array Api (value,index)
+        return (
+          <li key={index}>
+            <TodoItem todo={value}
+              onDelete={this.delete}
+              onToggle={this.toggle} />
+          </li>
+        )
+      })
     return (
       <div className="App">
         <h1>
-          {this.state.user.username||'我'}的待办
-          {this.state.user.id?<button onClick={this.signOut}>登出</button>:null}
+          {this.state.user.username || '我'}的待办
+          {this.state.user.id ? <button onClick={this.signOut}>登出</button> : null}
         </h1>
         <div className="inputWrapper">
-          <TodoInput 
-          onSubmit={this.addTodo}
-          onChange={this.changeTitle}
-          content={this.state.newTodo}/>
+          <TodoInput
+            onSubmit={this.addTodo}
+            onChange={this.changeTitle}
+            content={this.state.newTodo} />
         </div>
         <ol className="todoList">
           {todos}
         </ol>
-        {this.state.user.id?null:<UserDialog onSignIn={this.onSignUpOrSignIn} onSignUp={this.onSignUpOrSignIn}/>}
+        {this.state.user.id ? null : <UserDialog onSignIn={this.onSignUpOrSignIn} onSignUp={this.onSignUpOrSignIn} />}
       </div>
     );
   }
